6c03757a5fd472f5ad305aea496cb958db6b7733
Johnny Bowman <johntbowman.production@gmail.com>
2022-01-12 20:25:07 -0700

    Complete index.md titles.rb challenge

---
+++ challenge/titles.rb
@@ -0,0 +1,16 @@
+# We chose 'titles' because it relates to the content within, in this case book titles
+# books variable declares string "Harry Potter"
+books = "Harry Potter"
+# variable harry_potter stores integer '7' as number of books in the series
+harry_potter = 7
+# potter_good is boolean declared as "true" because that's a fact
+potter_good = true
+
+book_1 = "The Sorcerers Stone"
+
+book_2 = "The Chamber of Secrets"
+
+book_3 = "The Prisoner of Azkaban"
+# more_books is an array of books
+# there are three elements in this array
+more_books = ["East of Eden", "Cat in the Hat", "Green Eggs & Ham"]
